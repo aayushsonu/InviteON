4649b314b58f02183ccc166b2ffa64dca442c971
aayushsonu <engineer.aayush1234@gmail.com>
2020-06-21 14:13:26 +0530

    Now our project is compleated statically, Hope we give some more time on it to make it a real world dynamic project...

--- app/src/main/java/com/snifyak/inviteon/Splash_screen.java
+++ app/src/main/java/com/snifyak/inviteon/Splash_screen.java
@@ -5,18 +5,35 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.WindowManager;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Splash_screen extends AppCompatActivity {
-//    MediaPlayer ourSound;
+
+    MediaPlayer oursound;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splashscreen);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-//        ourSound = MediaPlayer.create(this,R.raw)
 
-        startActivity(new Intent(this,MainActivity.class));
+        oursound =MediaPlayer.create(this,R.raw.splashclip);
+        oursound.start();
+        oursound.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Splash_screen.this.oursound.stop();
+                Splash_screen.this.startActivity(new Intent(Splash_screen.this,MainActivity.class));
+                Splash_screen.this.finish();
+            }
+        });
 
+//        startActivity(new Intent(this,MainActivity.class));
+//        finish();
+
+    }
+    public void onDestroy(){
+        this.oursound.stop();
+        super.onDestroy();
     }
 }
